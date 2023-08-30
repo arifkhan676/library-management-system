4f8ce42aa9367b977eeaebe78f11b9727aa25256
arifkhan676 <arifulhasan5500@gmail.com>
2023-08-31 00:49:12 +0600

    google authenication done

--- src/Components/Header/NavbarMenu.js
+++ src/Components/Header/NavbarMenu.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
 import Button from 'react-bootstrap/Button';
 import { NavLink } from 'react-router-dom';
-
+import { auth, provider } from '../../firebase.config';
+import { signInWithPopup } from 'firebase/auth';
 
 const NavbarMenu = () => {
+
+    const [googleData, setGoogleData] = useState(
+        {
+            isLogin: false,
+            name: '',
+            email: '',
+            photoURL: ''
+        }
+    );
+
+
+    const googleLogin = () => {
+        signInWithPopup(auth, provider).then(res => {
+            const { displayName, email, photoURL } = res.user;
+            // console.log(res.user)
+            const newUser = {
+                isLogin: true,
+                name: displayName,
+                email: email,
+                photoURL: photoURL
+            }
+            setGoogleData(newUser)
+
+        })
+    }
+    console.log(googleData);
+
     return (
         <div>
             <nav className='navbar navbar-expand-lg' >
@@ -30,14 +58,18 @@ const NavbarMenu = () => {
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Seat Book</a>
                             </li>
+                            <li class="nav-item">
+                                {googleData.isLogin && <img style={{ width: '50px', borderRadius: '25px' }} src={googleData.photoURL} alt="" />}
+                            </li>
                         </ul>
                         <div className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="www.google.com" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <Button variant="outline-dark">Log In</Button>
+                                <Button variant="outline-dark">Users</Button>
                             </a>
                             <div className="dropdown-menu " aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="www.google.com">Student Login</a>
-                                <a className="dropdown-item" href="www.google.com">Admin Login</a>
+                                <button className="dropdown-item" onClick={googleLogin} >
+                                    {googleData.isLogin === false ? 'Login' : 'Logout'}
+                                </button>
                             </div>
                         </div>
                     </div>
---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { getAuth, GoogleAuthProvider, initializeAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCRgCosooY1-KWn8Zu8SAjwDPsGg_eeNZs",
+    authDomain: "iphone-store-test-8343a.firebaseapp.com",
+    projectId: "iphone-store-test-8343a",
+    storageBucket: "iphone-store-test-8343a.appspot.com",
+    messagingSenderId: "643407879586",
+    appId: "1:643407879586:web:cad0ebe11762c0acd626fb"
+};
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+export { auth, provider }
