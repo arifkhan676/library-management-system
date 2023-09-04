60cb5b289c5e92339cbcd10111fa2bae3649355a
arifkhan676 <arifulhasan5500@gmail.com>
2023-09-04 22:28:40 +0600

    authencate processing

--- src/Components/PrivateRoute/PrivateRoute.js
+++ src/Components/PrivateRoute/PrivateRoute.js
@@ -1,17 +1,15 @@
 import React, { useContext } from 'react'
-import { Route, Redirect } from 'react-router-dom'
+import { Route, Redirect, Navigate } from 'react-router-dom'
 import { ContextAPI } from '../../App'
 
 const PrivateRoute = ({ children, ...rest }) => {
     const [googleData, setGoogleData] = useContext(ContextAPI)
 
     return (
-        <div>
-            <Route {...rest} render={({ location }) => (
-                googleData.email ? (children) : (<Redirect to={{ pathname: '/Login', state: { from: location } }} />)
-            )}
-            />
-        </div>
+        <Route {...rest} render={({ location }) => (
+            googleData.email ? (children) : (<Navigate to='/Login' />)
+        )}
+        />
     )
 }
 
